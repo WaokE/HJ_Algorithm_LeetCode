de6e9b9b3b4ab2cddfe0ce14cdef3b1e940583d6
WaokE <128684924+WaokE@users.noreply.github.com>
2024-02-24 22:30:07 +0900

    Time: 57 ms (27.20%), Space: 48.7 MB (38.71%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    const kelvin = celsius + 273.15;
+    const fahrenheit = (celsius * 9/5) + 32;
+
+  return [ kelvin, fahrenheit ];
+};
